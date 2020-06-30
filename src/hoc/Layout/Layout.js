9553253df225c0108b739975c620855c9e4c5cb5
monicafu <31578677+monicafu@users.noreply.github.com>
2020-06-30 15:08:17 -0700

    added token access to orders and connect layout to signin

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
@@ -9,27 +11,37 @@ class Layout extends Component {
   }
 
   sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false});
+    this.setState({ showSideDrawer: false });
   }
 
   sideDrawerToggledHandler = () => {
-    this.setState( (prevState) => {
+    this.setState((prevState) => {
       return { showSideDrawer: !prevState.showSideDrawer };
-    } );
+    });
   }
 
-  render () {
+  render() {
     return (
       <Fragment>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggledHandler}/>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
           {this.props.children}
         </main>
       </Fragment>
-    )};
+    )
+  };
 }
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+      isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect( mapStateToProps )( Layout );
